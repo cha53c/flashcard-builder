1139a690bddc8d706df1cc4aeee5caa5f8ed6855
chrischase <chr15t0pher.cha53@gmail.com>
2019-01-10 22:07:35 +0000

    added http call to oxford dict

--- server/main.js
+++ server/main.js
@@ -1,5 +1,26 @@
 import { Meteor } from 'meteor/meteor';
+import { HTTP } from 'meteor/http';
 
 Meteor.startup(() => {
   // code to run on server at startup
+});
+
+Meteor.methods({
+  hello(){
+    console.log('Hello World');
+    HTTP.call('GET', 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace',
+    { headers:
+      {
+      'Accept': 'application/json',
+      'app_id': 'blah',
+      'app_key': 'blah'
+      }
+    }, function(err, res){
+      if(err){
+        console.log(err);
+      } else {
+        console.log(res);
+      }
+    });
+  }
 });
