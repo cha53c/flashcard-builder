251dc170b8d80ce7b365b17d72b1ac51bc5b1e36
chrischase <chr15t0pher.cha53@gmail.com>
2019-01-13 12:22:37 +0000

    refactored tests

--- tests/dictionary.test.js
+++ tests/dictionary.test.js
@@ -1,6 +1,9 @@
+import assert from "assert";
+import '/imports/server/dictionary-utils';
+
 describe('dictionary utils', function () {
   it('builds new url', function () {
     var res = buildURL('myurl', 'myword');
     assert.equal(res, 'myurl/myword');
   })
-})
+});
--- tests/main.js
+++ tests/main.js
@@ -1,5 +1,5 @@
 import assert from "assert";
-import '/server/main.js'
+import '/tests/dictionary.test.js'
 
 describe("flashcard-builder", function () {
   it("package.json has correct name", async function () {
@@ -16,12 +16,6 @@ describe("flashcard-builder", function () {
   if (Meteor.isServer) {
     it("server is not client", function () {
       assert.strictEqual(Meteor.isClient, false);
-    });
-    describe('dictionary utils', function () {
-      it('builds new url', function () {
-        var res = buildURL('myurl', 'myword');
-        assert.equal(res, 'myurl/myword');
-      });
     });
   }
 });
