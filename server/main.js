14ce04891363b201a8a0fe2b550c9090600f2b01
chrischase <chr15t0pher.cha53@gmail.com>
2019-01-13 10:31:28 +0000

    updated to use setting for external resources

--- server/main.js
+++ server/main.js
@@ -8,19 +8,36 @@ Meteor.startup(() => {
 Meteor.methods({
   hello(){
     console.log('Hello World');
-    HTTP.call('GET', 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace',
-    { headers:
-      {
-      'Accept': 'application/json',
-      'app_id': 'blah',
-      'app_key': 'blah'
-      }
-    }, function(err, res){
-      if(err){
-        console.log(err);
-      } else {
-        console.log(res);
-      }
-    });
+
+    getDefinitionFromOED();
+
   }
 });
+
+function getDefinitionFromOED(){
+
+  var newUrl = buildURL(Meteor.settings.oed.entries_url, 'orca');
+  console.log('newUrl: ' + newUrl);
+  // 'https://od-api.oxforddictionaries.com:443/api/v1/entries/en/ace'
+  HTTP.call('GET', newUrl,
+  { headers:
+    {
+    'Accept': 'application/json',
+    'app_id': Meteor.settings.oed.app_id,
+    'app_key': Meteor.settings.oed.app_key
+    }
+  }, function(err, res){
+    if(err){
+      console.log(err);
+    } else {
+      console.log(res);
+    }
+  });
+}
+
+function buildURL (url, word){
+  // // TODO: is there a built are way to log params?
+  console.log('params:' + url +' ' + word);
+
+  return url.concat('/'+ word);
+}
